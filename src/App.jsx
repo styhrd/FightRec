e2d2646d1c11dda868717785596d2c9470876445
styhrd <juanmiguelcaparas76@gmail.com>
2024-11-26 18:31:42 +0800

    Tailwind Dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Navbar from "./components/Navbar"
 
 
 function App() {
@@ -5,7 +6,12 @@ function App() {
 
   return (
     <>
-     
+      <div className="h-[100vh] w-full p-3 text-6xl" >
+        
+        <div>
+          <Navbar/>
+        </div>
+      </div>
     </>
   )
 }
