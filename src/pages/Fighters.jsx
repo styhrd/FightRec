43bcffa1a582e56e2775c0f717ced9c7b9b9035e
styhrd <juanmiguelcaparas76@gmail.com>
2024-12-03 16:48:49 +0800

    Fighters with pagination

--- src/pages/Fighters.jsx
+++ src/pages/Fighters.jsx
@@ -1,23 +1,30 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchByCat } from '../store/fightersSlice'
 
 const Fighters = () => {
   const [category, setCategory] = useState('Heavyweight')
+  const dispatch = useDispatch()
+  const { fighters } = useSelector((state) => state.fighters)
+  const [currentPage, setCurrentPage] = useState(1); // Current page number
+  const itemsPerPage = 10;
+
+  const totalPages = Math.ceil(fighters.length / itemsPerPage);
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentData = fighters.slice(indexOfFirstItem, indexOfLastItem);
 
 
-  // useEffect(async () => {
-  //   const response = await axios.get('https://v1.mma.api-sports.io/fighters', {
-  //     headers: {
-  //       'x-rapidapi-host': 'v1.mma.api-sports.io',
-  //       'x-rapidapi-key': '3f8a88f0952f6d91f600e863af380710'
-  //     },
-  //     params: {
-  //       category: category
-  //     }
-  //   })
-  //   console.log(response);
-
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+  // useEffect(() => {
+  //   dispatch(fetchByCat(category))
   // }, [category])
+
+  console.log(fighters);
+
 
   return (
     <>
@@ -31,6 +38,27 @@ const Fighters = () => {
         <option value="MiddleWeight">MiddleWeight</option>
         <option value="Welterweight">Welterweight</option>
       </select>
+
+      <div>
+        {currentData.map((fighter) => {
+          return (
+            <li>{fighter.name}</li>
+          )
+
+        })}
+      </div>
+
+      <div className="pagination">
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            key={index}
+            onClick={() => handlePageChange(index + 1)}
+            className={currentPage === index + 1 ? "active" : ""}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
 
     </>
   )
--- src/store/fightersSlice.jsx
+++ src/store/fightersSlice.jsx
@@ -4,13 +4,35 @@ import axios from "axios";
 const api_key = import.meta.env.VITE_API_KEY
 const fighterUrl = 'https://v1.mma.api-sports.io/fighters'
 
+export const fetchByCat = createAsyncThunk('fighters/fetchbycat',
+    async (categ) => {
+        const response = await axios.get(fighterUrl, {
+            params: {
+                category: categ
+            },
+            headers: {
+                'x-rapidapi-host': 'v1.mma.api-sports.io',
+                'x-rapidapi-key': api_key
+            }
 
+        })
+
+        return response.data.response || []
+    }
+)
 
 
 const fightersSlice = createSlice({
     name: "fighters",
     initialState: {
         fighters: []
+    },
+
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchByCat.fulfilled, (state, action) => {
+                state.fighters = action.payload
+            })
     }
 })
 
